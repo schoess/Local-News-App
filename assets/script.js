be015e6c4af36da7574659ac4dd1d3dd0712c7fa
Jacob Kyle <jkthomps21@gmail.com>
2020-05-10 18:08:08 -0500

    Updated HTML and JS

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,5 @@
 // This is for the sidenav
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  // var instances = M.Sidenav.init(elems, options);
-});
-
-
-
 $(document).ready(function(){
   $('.sidenav').sidenav();
   $('.dropdown-trigger').dropdown();
